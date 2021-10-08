eda975c3dbdeaac7d1e49499f7e53b06e70462d2
Carlos Pavão <carlos.henrique93@msn.com>
2021-10-08 11:14:51 -0300

    atualização front

--- src/main/java/workflix/com/github/workflix/Controller/UsuarioController.java
+++ src/main/java/workflix/com/github/workflix/Controller/UsuarioController.java
@@ -3,11 +3,29 @@ package workflix.com.github.workflix.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/user")
 public class UsuarioController {
 
     @GetMapping("/login")
-    void logar(){
-        return ;
+    public String Login() {
+        return "/usuario/login";
     }
+
+    @GetMapping("/cadastrar")
+    public String Cadastrar() {
+        return "/Usuario/newUser";
+    }
+
+
+    @GetMapping("/listar")
+    public String Listar() {
+        return "/funcionario/lista";
+    }
+
+
 }
